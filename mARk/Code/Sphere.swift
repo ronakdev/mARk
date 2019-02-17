274bd3979e84f815c480f24f4057ddeb5d43245e
Ronak Shah <ronakshahultimate@gmail.com>
2019-02-17 04:33:26 -0800

    MVP but better (all thanks to Daniel and Nathan)

---
+++ mARk/Code/Sphere.swift
@@ -0,0 +1,122 @@
+//
+//  Sphere.swift
+//  mARk
+//
+//  Created by Ronak Shah on 2/16/19.
+//  Copyright © 2019 Ronak Shah. All rights reserved.
+//
+
+import Foundation
+import ARKit
+import FirebaseFirestore
+
+class Sphere: SCNNode {
+    
+    static let radius: CGFloat = 0.01
+    var color: String = "white"
+    let uicolor : UIColor = UIColor(red: 1, green: 165/255, blue: 0, alpha: 1)
+    
+    let sphereGeometry: SCNSphere
+    let sphereNode: SCNNode!
+    var positionString: String
+    
+    // Required but unused
+    required init?(coder aDecoder: NSCoder) {
+        sphereGeometry = SCNSphere(radius: Sphere.radius)
+        self.positionString = ""
+        self.sphereNode = nil
+        super.init(coder: aDecoder)
+        self.positionString = "\(self.position.x),\(self.position.y),\(self.position.z)"
+    }
+    
+    // The real action happens here
+    init(position: SCNVector3) {
+        self.sphereGeometry = SCNSphere(radius: Sphere.radius)
+        self.positionString = "\(position.x),\(position.y),\(position.z)"
+        self.sphereNode = SCNNode(geometry: self.sphereGeometry)
+
+        super.init()
+        
+        sphereNode.position = position
+        
+        self.addChildNode(sphereNode)
+//        self.color = color;
+    }
+    
+    func clear() {
+        self.removeFromParentNode()
+    }
+    
+    func toDictionary(lat: Double, lng: Double) -> [String: Any] {
+        var dict: [String: Any] = [:]
+        
+        dict["color"] = self.color
+        dict["hardLocation"] = GeoPoint(latitude: lat, longitude: lng)
+        
+        dict["relativeLocation"] = ["x": "\(self.sphereNode.position.x)", "y": "\(self.sphereNode.position.y)", "z": "\(self.sphereNode.position.z)"]
+        dict["positionVector"] = "\(self.sphereNode.position.x),\(self.sphereNode.position.y),\(self.sphereNode.position.z)"
+        
+        // do geohasing nonsense here
+        
+        return dict
+    }
+    
+    func setEditable(_ editable: Bool) {
+        
+        let changeColorAction = SCNAction.run { (node) in
+            let material: SCNMaterial = SCNMaterial()
+            
+            if !editable {
+                material.diffuse.contents = UIColor.red
+            } else {
+                material.diffuse.contents = self.uicolor
+                self.color = self.toHexString(color: self.uicolor)//self.StringFromUIColor(self.color)
+            }
+            self.sphereGeometry.materials = [material]
+            self.sphereNode.geometry = self.sphereGeometry
+        }
+        let waitAction = SCNAction.wait(duration: 1)
+        let revertColorAction = SCNAction.run { (node) in
+            let material: SCNMaterial = SCNMaterial()
+            
+            if editable {
+                material.diffuse.contents = UIColor.red
+            } else {
+                material.diffuse.contents = self.StringFromUIColor(self.color)
+            }
+            self.sphereGeometry.materials = [material]
+            self.sphereNode.geometry = self.sphereGeometry
+        }
+        self.runAction(SCNAction.sequence([changeColorAction, waitAction, revertColorAction]))
+    }
+    
+    func StringFromUIColor(_ name: String) -> UIColor? {
+        let selector = Selector("\(name)Color")
+        if UIColor.self.responds(to: selector) {
+            let color = UIColor.self.perform(selector).takeUnretainedValue()
+            return (color as! UIColor)
+        } else {
+            return nil
+        }
+    }
+    
+    func setUIColor(color : UIColor) {
+        sphereGeometry.firstMaterial?.diffuse.contents = color
+        self.color = toHexString(color : color)
+    }
+    
+    func toHexString(color : UIColor) -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        
+        color.getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgba:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0 | (Int)(a*255)
+        
+        return NSString(format:"#%08x", rgba) as String
+    }
+
+
+}
--- mARk/Code/ViewController.swift
+++ mARk/Code/ViewController.swift
@@ -11,17 +11,38 @@ import SceneKit
 import ARKit
 import Firebase
 import CoreLocation
+import ColorSlider
 
 class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
 
+    
+    // Nathan/Daniel master plan:
+        // store a set location for each mark
+        // load the mark if user starts the app within the set location radius
+        // using distanceFromLocation method to find distance from two points
+    
+    
+//    latitude: 33.6489, longitude: -117.8421
     @IBOutlet var sceneView: ARSCNView!
     var spheres: [Sphere] = []
-    var currentLocation: CLLocation! = CLLocation(latitude: 33.6489, longitude: -117.8421)
+    var currentLocation: CLLocation! = CLLocation(latitude: 33.6489, longitude: -117.8421  ) // utc not where i am
     var locManager: CLLocationManager!
+    let setLat = 33.64774
+    let setLong = -117.83562
+    @IBOutlet weak var colorSliderView: UIView!
+    var colorSlider: ColorSlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorSlider = ColorSlider(orientation: .horizontal, previewSide: .top)
         
+        colorSlider.frame = CGRect(x: 25, y: self.sceneView.frame.height - 12, width: self.sceneView.frame.width - 20, height: 12)
+        colorSlider.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 20)
+        colorSlider.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 20)
+        
+        sceneView.addSubview(colorSlider)
+        
+        colorSlider.addTarget(self, action: #selector(changedColor(_:)), for: .valueChanged)
         locManager = CLLocationManager()
         locManager.requestWhenInUseAuthorization()
         
@@ -32,10 +53,10 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        let scene = //SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+//        sceneView.scene = scene
         
         let tapRecog = UITapGestureRecognizer(target: self, action: #selector(self.didTapScreen(_:)))
         self.sceneView.addGestureRecognizer(tapRecog)
@@ -44,12 +65,15 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
         
         let db = Firestore.firestore()
         
+        // THIS IS HARD CODED RIGHT NOW
         let latitude = self.currentLocation.coordinate.latitude
         let longitude = self.currentLocation.coordinate.longitude
         
         let lat = 0.0144927536231884
         let lon = 0.0181818181818182
         let distance = 0.0189394
+        
+        
         
         let lowerLat = latitude - (lat * distance)
         let lowerLon = longitude - (lon * distance)
@@ -59,38 +83,47 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
 
         print("latitude: \(latitude)")
         print("longitude: \(longitude)")
-        let ourSetDistance = sqrt( (latitude)**2 + (longitude)**2)
         
         let markQuery = db.collection("marks")
-        //.whereField(sqrt("setDistance")**2 + ourSetDistance**2), "
-        //.whereField("lat", isLessThan: greaterLat).whereField("lat", isGreaterThan: lowerLat)
+
         
         markQuery.getDocuments { (snapshot, error) in
             if let error = error {
                 print("Error getting documents: \(error)")
             } else {
                 
+                var marksFound = 0
                 for document in snapshot!.documents {
                     print("\(document.documentID) => \(document.data())")
                     let nodes = document.data()["mark"] as! [[String: Any]]
-                    for data in nodes {
-//                        let xData = ((data["relativeLocation"] as! [String: Any])["x"]) as! Float
-                        
-//                        print(xData)
-                        
-                        
-                        self.addSphere(position:
-                            SCNVector3(
-                                Float((data["relativeLocation"] as! [String: Any])["x"] as! String)!,
-                                Float((data["relativeLocation"] as! [String: Any])["y"] as! String)!,
-                                Float((data["relativeLocation"] as! [String: Any])["z"] as! String)!
+                    let markData = document.data()
+                    
+                    // creates CLLocation for each mark
+                    let markLocation: CLLocation! = CLLocation(latitude: markData["lat"] as! Double, longitude: markData["lng"]! as! Double  )
+                    
+                    // the value 1 can be tested
+                    if (self.currentLocation.distance(from: markLocation) < 1){
+                        marksFound+=1
+                        for data in nodes {
+                            self.addSphere(position:
+                                SCNVector3(
+                                    Float((data["relativeLocation"] as! [String: Any])["x"] as! String)!,
+                                    Float((data["relativeLocation"] as! [String: Any])["y"] as! String)!,
+                                    Float((data["relativeLocation"] as! [String: Any])["z"] as! String)!
+                                )
                             )
-                        )
-                        self.spheres = []
+                            self.spheres[0].setUIColor(color: self.hexToUIColor(hexString: data["color"] as! String))
+                            self.spheres = []
+                        }
                     }
                 }
+                print("\n\n\n\n\n\(marksFound) total marks found\n\n\n\n\n\n\n")
             }
         }
+    }
+    
+    @objc func changedColor(_ slider: ColorSlider) {
+        print (slider.color)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -105,7 +138,10 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
     
     @objc func didTapScreen(_ recog: UITapGestureRecognizer) {
         let location = recog.location(in: self.sceneView)
-        print("X: \(location.x), Y: \(location.y)")
+        
+        if (location.y > 600) {
+            return
+        }
         
         if let cameraNode = self.sceneView.pointOfView {
             
@@ -126,9 +162,11 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
             }
             
             let db = Firestore.firestore()
-            let markUUID = "mark"
-            db.collection("marks").addDocument(data: [markUUID : nodes, "lat" : self.currentLocation.coordinate.latitude, "lng" : self.currentLocation.coordinate.latitude])
-            
+            db.collection("marks").addDocument(data: [
+            "mark" : nodes,
+            "lat" : self.currentLocation.coordinate.latitude,
+            "lng" : self.currentLocation.coordinate.longitude,
+            ])
             self.spheres = [] // we forget about these now
         }
     }
@@ -143,6 +181,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
     func addSphere(position: SCNVector3) {
         print("adding sphere at point: \(position)")
         let sphere: Sphere = Sphere(position: position)
+        sphere.setUIColor(color: colorSlider.color)
         self.sceneView.scene.rootNode.addChildNode(sphere)
         // if we keep an array of these babies, then calling
         // sphere.clear() on each will remove them from the scene
@@ -155,7 +194,18 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
         // to: nil is automatically scene space
         return scenePosition
     }
+    
+    
+//    func UIColorFromString(string: String) -> UIColor {
+//        let componentsString = string.replace("[", withString: "").replace("]", withString: "")
+//        let components = componentsString.componentsSeparatedByString(", ")
+//        return UIColor(red: CGFloat((components[0] as NSString).floatValue),
+//                       green: CGFloat((components[1] as NSString).floatValue),
+//                       blue: CGFloat((components[2] as NSString).floatValue),
+//                       alpha: CGFloat((components[3] as NSString).floatValue))
+//    }
 
+    
     // MARK: - ARSCNViewDelegate
     
 /*
@@ -191,5 +241,30 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
         if (CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() == .authorizedAlways) {
             currentLocation = manager.location
         }
+    }
+    func hexToUIColor(hexString: String) -> UIColor {
+        let r, g, b, a: CGFloat
+        
+        if hexString.hasPrefix("#") {
+        let start = hexString.index(hexString.startIndex, offsetBy: 1)
+        let hexColor = String(hexString[start...])
+        
+        if hexColor.count == 8 {
+        let scanner = Scanner(string: hexColor)
+        var hexNumber: UInt64 = 0
+        
+        if scanner.scanHexInt64(&hexNumber) {
+        r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+        g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+        b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+        a = CGFloat(hexNumber & 0x000000ff) / 255
+        
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+        
+        }
+        }
+        }
+        
+        return UIColor(red: 0, green: 0, blue: 0, alpha: 1)
     }
 }
